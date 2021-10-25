b2c2741d79173f10238c4cfcd00a8c4254e70dc4
Adrian Escalada <it@jc-mc.com>
2021-10-25 10:35:04 +0200

    Refactor if_else

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -23,21 +23,21 @@ class GildedRose
     {
         if ($item->name == 'Aged Brie') {
             if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
+                $this->increaseQuality($item);
             }
             $item->sell_in = $item->sell_in - 1;
             if ($item->sell_in < 0 && $item->quality < 50) {
-                $item->quality = $item->quality + 1;
+                $this->increaseQuality($item);
             }
         } elseif ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
             if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
+                $this->increaseQuality($item);
             }
             if ($item->sell_in < 11 && $item->quality < 50) {
-                $item->quality = $item->quality + 1;
+                $this->increaseQuality($item);
             }
             if ($item->sell_in < 6 && $item->quality < 50) {
-                $item->quality = $item->quality + 1;
+                $this->increaseQuality($item);
             }
             $item->sell_in = $item->sell_in - 1;
             if ($item->sell_in < 0) {
@@ -47,12 +47,22 @@ class GildedRose
 
         } else {
             if ($item->quality > 0) {
-                $item->quality = $item->quality - 1;
+                $this->decreaseQuality($item);
             }
             $item->sell_in = $item->sell_in - 1;
             if ($item->sell_in < 0 && $item->quality > 0) {
-                $item->quality = $item->quality - 1;
+                $this->decreaseQuality($item);
             }
         }
+    }
+
+    protected function increaseQuality($item): void
+    {
+        $item->quality = $item->quality + 1;
+    }
+
+    protected function decreaseQuality($item): void
+    {
+        $item->quality = $item->quality - 1;
     }
 }
